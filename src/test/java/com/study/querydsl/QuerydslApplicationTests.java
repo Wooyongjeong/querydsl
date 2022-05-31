048be6f7b124d6c9c806d264c4cf4229f17918c4
wooyongjeong <wooyong.dev@gmail.com>
2022-06-01 01:42:22 +0900

    [프로젝트 환경설정] 스프링 부트 설정 - JPA, DB

--- src/test/java/com/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -6,6 +6,7 @@ import com.study.querydsl.entity.QHello;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
 	@PersistenceContext
